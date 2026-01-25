e4131a04be39b119bec0e443882a9093704b8c88
KirilStefanovski226037 <kstef3849@gmail.com>
2026-01-25 18:08:38 +0100

    test start btn

--- main_menu.gd
+++ main_menu.gd
@@ -11,9 +11,11 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	print("Start pressed")
+	get_tree().change_scene_to_file("res://scenes/gameplay_scene_1.tscn")
 
 func _on_settings_pressed() -> void:
 	print("Settings pressed")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+	
