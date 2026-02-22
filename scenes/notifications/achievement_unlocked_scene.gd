d141f2e8ffb7812852f62775009da01fbd7930f4
KirilStefanovski226037 <kstef3849@gmail.com>
2026-02-22 23:05:58 +0100

    added achievements :3

--- scenes/notifications/achievement_unlocked_scene.gd
+++ scenes/notifications/achievement_unlocked_scene.gd
@@ -1,14 +1,12 @@
 extends Node2D
 signal close
 
-@onready var achievement_name_label = $BackgroundBackButton/TextureRect/achievement_name_label
 @onready var achievementTR = $BackgroundBackButton/TextureRect/TextureRect
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func initialize(accessory_data: AccessoryData):
-	achievement_name_label.text = accessory_data.name
 	achievementTR.texture = accessory_data.texture
 
 
