a0d7280911d819a633ee5f18fc0145fb3a9f5824
Dimitri Petrovski <dimitri.petrovski2@gmail.com>
2026-02-22 19:55:30 +0100

    Resolving stash murges

---
+++ scenes/desk_accessories.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _ready():
+	_refresh_accessories()
+	ShopGameData.equipment_changed.connect(_refresh_accessories)
+
+func _refresh_accessories():
+	for accessory_node in get_children():
+		# Ensure the node is actually visible to the engine logic
+		accessory_node.show() 
+		
+		if accessory_node.name in ShopGameData.equipped_items:
+			# Fully visible (Opaque)
+			accessory_node.modulate.a = 1.0
+		else:
+			# Fully invisible but still takes up space (Transparent)
+			accessory_node.modulate.a = 0.0
